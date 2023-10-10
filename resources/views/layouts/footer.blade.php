f2fa06e432cb5b161f0da8db8d228277ca356f77
HosnyFahim <hosny.fahim1990@gmail.com>
2023-10-10 15:53:11 +0200

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="bg-gray-700 py-15 mt-20">
+    <div class="container">
+        <h3>Pages</h3>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/">Blog</a></li>
+            <li><a href="/">login</a></li>
+            <li><a href="/">Register</a></li>
+        </ul>
+    </div>
+</footer>
